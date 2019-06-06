fff8cd89be4aa0cff28c008db0d68bbd1009fb45
Nithya Muthu <nithya.muthu2014@gmail.com>
2019-06-06 00:07:58 -0700

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+        name: 'quickview',
+        script: './index.js'
+    }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-34-203-219-158.compute-1.amazonaws.com',
+            key: '~/.ssh/quickviewawsinstance.pem',
+            ref: 'origin/master',
+            repo: 'git@github.com:NithyaMuthu/QvYappsService.git',
+            path: '/home/ubuntu/QvYappsService',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+}
